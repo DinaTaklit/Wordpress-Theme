eaae2a36c6d14af131caf7316e3f604d9bade9bf
DinaTaklit <ez_taklit@esi.dz>
2020-06-11 05:45:39 +0100

    feat: add header

---
+++ header.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Gutener
+ */
+
+?>
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="<?php bloginfo("charset"); ?>">
+    <title><?php bloginfo("name"); ?></title>
+    <link rel="pingback" href="<?php bloginfo("pingback_url"); ?>">
+</head>
