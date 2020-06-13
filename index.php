ef9fc2fc309c24904c9fa303a4e8f6c5d191203c
DinaTaklit <ez_taklit@esi.dz>
2020-06-13 07:25:08 +0100

    feat: Design Blog Posts Part 1

--- index.php
+++ index.php
@@ -14,8 +14,58 @@
 
 get_header();
 ?>
-
-
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="main-post">
+                <h3 class="post-title"> Lean Html </h3>
+                <span class="post-author"> Dina</span>
+                <span class="post-date"> 13/06/2020</span>
+                <span class="post-comments"> 20 comments</span>
+                 <img class="post-img img-fluid img-thumbnail" src="http://placehold.it/300x100/300" alt="">
+                <p class="post-content"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem soluta repudiandae eum natus iure doloribus excepturi veniam libero minima fugiat, repellat, nulla dolorum quod! Iusto quibusdam iure distinctio accusantium hic.Beatae ipsum nam fugit delectus quas voluptas.
+                </p>
+                <p class="post-categories">html, css</p>
+            </div>
+        </div>  
+        <div class="col-sm-6">
+            <div class="main-post">
+                <h3 class="post-title"> Lean Html </h3>
+                <span class="post-author"> Dina</span>
+                <span class="post-date"> 13/06/2020</span>
+                <span class="post-comments"> 20 comments</span>
+                 <img class="post-img img-fluid img-thumbnail" src="http://placehold.it/300x100/300" alt="">
+                <p class="post-content"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem soluta repudiandae eum natus iure doloribus excepturi veniam libero minima fugiat, repellat, nulla dolorum quod! Iusto quibusdam iure distinctio accusantium hic.Beatae ipsum nam fugit delectus quas voluptas.
+                </p>
+                <p class="post-categories">html, css</p>
+            </div>
+        </div> 
+        <div class="col-sm-6">
+            <div class="main-post">
+                <h3 class="post-title"> Lean Html </h3>
+                <span class="post-author"> Dina</span>
+                <span class="post-date"> 13/06/2020</span>
+                <span class="post-comments"> 20 comments</span>
+                 <img class="post-img img-fluid img-thumbnail" src="http://placehold.it/300x100/300" alt="">
+                <p class="post-content"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem soluta repudiandae eum natus iure doloribus excepturi veniam libero minima fugiat, repellat, nulla dolorum quod! Iusto quibusdam iure distinctio accusantium hic.Beatae ipsum nam fugit delectus quas voluptas.
+                </p>
+                <p class="post-categories">html, css</p>
+            </div>
+        </div> 
+        <div class="col-sm-6">
+            <div class="main-post">
+                <h3 class="post-title"> Lean Html </h3>
+                <span class="post-author"> Dina</span>
+                <span class="post-date"> 13/06/2020</span>
+                <span class="post-comments"> 20 comments</span>
+                <img class="post-img img-fluid img-thumbnail" src="http://placehold.it/300x100/300" alt="">
+                <p class="post-content"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem soluta repudiandae eum natus iure doloribus excepturi veniam libero minima fugiat, repellat, nulla dolorum quod! Iusto quibusdam iure distinctio accusantium hic.Beatae ipsum nam fugit delectus quas voluptas.
+                </p>
+                <p class="post-categories">html, css</p>
+            </div>
+        </div>    
+    </div>
+</div>
 
 <?php
 get_footer();
